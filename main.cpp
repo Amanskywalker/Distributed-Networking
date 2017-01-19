9b01188f9df37534b2a15b0c3b48b1223d74d9bf
Amanskywalker <ak47su30ac@gmail.com>
2017-01-19 23:56:19 +0530

    Adding initials codes 	new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Communication between two user without the use of and third party or any forwarding server etc.
+
+// Libraries Hearders
+#include <iostream>
+#include <cstdio> // to use c I/O if might needed in any screnario
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  cout>>"Hello";
+  return 0;
+}

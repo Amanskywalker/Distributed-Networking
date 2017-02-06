d25462f8b106ad4d6b3e385bcfd043cb12c14a4f
Amanskywalker <ak47su30ac@gmail.com>
2017-02-06 23:16:02 +0530

    added new functions
      int readline(int fd, char *ptr, int maxlen)
      Read a line from a descriptor. Read the line one byte at a time, looking for the newline. Then newline is stored in the buffer,
      then follow it with a null, returns the number of characters up to, but not including, the null
    
      str_cli(int *fd, int sockfd)
      Read the contents of the FILE *fp, write each line to the stream socket (to the server),
      then read a line back from the socket and write it to the standard output
     	modified:   echo_common.c

--- echo_common.c
+++ echo_common.c
@@ -1,6 +1,7 @@
 // this hold the common function in both the server and client programs
 
 // libraries
+#include <stdio.h>
 
 // read "n" bytes from the descriptor
 // Use in the place of read() when fd is a stream socket
@@ -42,6 +43,38 @@ int writen(int fd, char *ptr, nbytes)
   return (nbytes - nleft);
 }
 
+/* Read a line from a descriptor. Read the line one byte at a time, looking for the newline. Then newline is stored in the buffer,
+ then follow it with a null, returns the number of characters up to, but not including, the null
+*/
+
+int readline(int fd, char *ptr, int maxlen)
+{
+  int n, rc;
+  char c;
+  for (n = 1; n < maxlen; n++)
+  {
+    if ((rc= read(fd, &c, 1)) == 1)
+    {
+      *ptr++ = c;
+      if(c == '\n')
+          break;
+    }
+    else if (rc == 0 )
+    {
+      if (n == 1)
+        return 0 ;      // EOF, no data read
+      else
+        break ;         // EOF, some data was read
+    }
+    else
+        return (-1);    // error
+  }
+
+  *ptr = 0;
+  return (n);
+
+}
+
 #define MAXLINE 512
 
 str_echo(int stockfd)
@@ -60,4 +93,28 @@ str_echo(int stockfd)
     if(writen (sockfd, line, n) != n)
         err_dump("str_echo : writen error");
   }
+}
+
+str_cli(int *fd, int sockfd)
+{
+  int n;
+  char sendline[MAXLINE], recvline[MAXLINE];
+
+  while (fgets(sendline, MAXLINE, fp) != NULL)
+  {
+    n = strlen(sendline);
+    if (writen (sockfd, sendline, n) != n)
+        err_sys("str_cli : writen error on socket");
+    // now read a line form the socket and write it to the standard output
+
+    n = readline(sockfd, recvline, MAXLINE);
+    if (n < 0)
+        err_dump("str_cli: readline error");
+
+    recvline[n] = 0;
+    fputs(recvline, stdout);
+  }
+
+  if (ferror(fp))
+    err_sys("str_cli: error reading file");
 }
